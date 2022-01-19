512729a6b1c47baf44b39b4d60b3b0d24bec9ab5
Ana Dias <ana.diascarvalh@hotmail.com>
2022-01-19 14:18:35 -0300

    Finalizei as funções de cáculo no JS

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+let totalMedia = 0;
+
+// capturando os valores dos inputs
+const nota_linguagens = document.querySelector('#linguagens');
+const nota_humanas = document.querySelector('#humanas');
+const nota_natureza = document.querySelector('#natureza');
+const nota_matematica = document.querySelector('#matematica');
+const nota_redacao = document.querySelector('#redacao');
+// capturando o button
+const buttonCalcular = document.querySelector('.buttonCalcular');
+
+// definindo função média aritmética
+
+buttonCalcular.addEventListener('click', mediaAritmetica);
+
+function mediaAritmetica(){
+    const linguagens = Number(nota_linguagens.value);
+    const humanas = Number(nota_humanas.value);
+    const natureza = Number(nota_natureza.value);
+    const matematica = Number(nota_matematica.value);
+    const redacao = Number(nota_redacao.value);
+
+    const soma = linguagens + natureza + humanas + matematica + redacao;
+    const media = soma / 5;
+
+    totalMedia = media;
+    atualizarInterface();
+};
+
+// definindo função atualizar interface
+
+const resultadoNota = document.querySelector('.resultadoNota');
+
+function atualizarInterface(){
+    resultadoNota.innerText = `${totalMedia}`;
+};
